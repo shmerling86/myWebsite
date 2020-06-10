a256f6d2786e9639387b8538077ceba8f4e64fb6
shmerling86 <shmerling86@gmail.com>
2020-06-10 20:00:18 +0300

    init project

---
+++ src/app/app.module.ts
@@ -0,0 +1,29 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { WelcomeComponent } from './components/welcome/welcome.component';
+import { AboutComponent } from './components/about/about.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { PortfolioComponent } from './components/portfolio/portfolio.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavbarComponent,
+    WelcomeComponent,
+    AboutComponent,
+    ContactComponent,
+    PortfolioComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    ReactiveFormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/components/contact/contact.component.ts
@@ -0,0 +1,37 @@
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import emailjs, { EmailJSResponseStatus } from 'emailjs-com';
+
+
+@Component({
+  selector: 'app-contact',
+  templateUrl: './contact.component.html',
+  styleUrls: ['./contact.component.scss']
+})
+export class ContactComponent implements OnInit {
+
+  form: FormGroup;
+
+  constructor() { }
+
+  ngOnInit(): void {
+
+    this.form = new FormGroup({
+      firstName: new FormControl('', [Validators.minLength(2), Validators.maxLength(20), Validators.required]),
+      email: new FormControl('', [Validators.email, Validators.required]),
+      textarea: new FormControl('', [Validators.minLength(10), Validators.maxLength(200), Validators.required])
+    });
+  }
+
+  public sendEmail(e: Event) {
+    e.preventDefault();
+    emailjs.sendForm('gmail1', 'lior_shmerling_website', e.target as HTMLFormElement, 'user_XBFtu8NrVM6bYRsMdNuSE')
+      .then((result: EmailJSResponseStatus) => {
+        console.log(result.text);
+      }, (error) => {
+        console.log(error.text);
+      });
+    this.form.reset();
+  }
+
+}
